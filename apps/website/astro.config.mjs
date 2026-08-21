422939dee299ab0b550eecabfd660c7a735c8158
Himada4 <45938024+Himada4@users.noreply.github.com>
2026-08-21 18:53:36 +0200

    changed base link for images

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://bliss-and-innocence.github.io',
+  base: '/BlissAndInnocence',
+});
